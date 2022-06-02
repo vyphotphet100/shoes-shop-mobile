b088dc457a4cc770036b420b88afa5637d87da1f
Bao <baonguyen120301@gmail.com>
2022-06-02 20:15:32 +0700

    add Contact Us Activity

---
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/ContactUsActivity.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.caodinhsyvy_19110143.shoesshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
