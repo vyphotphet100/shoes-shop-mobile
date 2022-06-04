49b503a85b73bd5ec355018d01ec070d31a336a3
vyphotphet100 <caovy2001@gmail.com>
2022-06-04 15:10:28 +0700

    fix path activity

---
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/LoginActivity.java
@@ -0,0 +1,16 @@
+package hcmute.edu.vn.caodinhsyvy_19110143.shoesshop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
