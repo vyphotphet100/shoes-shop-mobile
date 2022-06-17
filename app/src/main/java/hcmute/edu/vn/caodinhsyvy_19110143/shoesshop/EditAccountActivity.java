95d7e19a1785876ac63dec526d47fa71bdb24879
vyphotphet100 <caovy2001@gmail.com>
2022-06-17 09:50:47 +0700

    fix confilct 16/6_vyphotphet branch

--- app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/EditAccountActivity.java
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/EditAccountActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import hcmute.edu.vn.caodinhsyvy_19110143.shoesshop.constant.AppConstant;
+
 public class EditAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);         //save instance state
-        setContentView(R.layout.activity_edit_account);     //view activity edit account
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+        mapping();
+        AppConstant.waitingAnimation(context, 800);
+        initSetupLayout();
+        initSetupData();
+
+        setEvent();
+
+    }
+
+    private void initSetupLayout() {
+        frmAccountLayContainer.removeAllViews();
+        frmAccountLayContainer.addView(accountAfterLoginCard.getView());
+
+        frmInfoLayContainer.removeAllViews();
+        frmInfoLayContainer.addView(informationCard.getView());
+
+        frameHeaderContainer.removeAllViews();
+        frameHeaderContainer.addView(headerCard.getView());
+    }
+
+    private void initSetupData() {
+        edtTxtFirstName.setText(AppConstant.loggedInUserEntity.getFirstName());
+        edtTxtLastName.setText(AppConstant.loggedInUserEntity.getLastName());
+        edtTxtPhone.setText(AppConstant.loggedInUserEntity.getPhone());
+        edtTxtEmail.setText(AppConstant.loggedInUserEntity.getEmail());
     }
 }
