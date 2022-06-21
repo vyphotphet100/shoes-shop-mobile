a0a83f80931904a12a9176da6642f5648cecb258
vyphotphet100 <caovy2001@gmail.com>
2022-06-21 13:54:30 +0700

    test commit

--- app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/card/AccountAfterLoginCard.java
+++ app/src/main/java/hcmute/edu/vn/caodinhsyvy_19110143/shoesshop/card/AccountAfterLoginCard.java
@@ -16,7 +16,7 @@ public class AccountAfterLoginCard extends BaseCard{
     private View view;
     private Context context;
 
-    // objects to map to ui of card - test
+    // objects to map to ui of card
     public TextView txtMyAccount, txtOrderHistory;
 
     //Function to map objects in code to associated views in UI
